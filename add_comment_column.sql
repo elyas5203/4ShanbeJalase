e92a55662cf5cf144c12e7281e36b303c16ccb5d
elyas5203 <elyas.bahmanabadi@gmail.com>
2026-09-17 22:00:42 +0330

    delet

--- add_comment_column.sql
+++
@@ -1,5 +0,0 @@
--- اضافه کردن ستون comment به جدول users
-ALTER TABLE users ADD COLUMN comment TEXT DEFAULT NULL;
-
--- به‌روزرسانی کاربران موجود با کامنت پیش‌فرض
-UPDATE users SET comment = 'کاربر عادی' WHERE comment IS NULL;
